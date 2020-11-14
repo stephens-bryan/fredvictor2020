bdece041dcf2b896d290c367b7a9abef6556283f
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-11-14 15:30:49 -0500

    added enqueue for stylesheet

--- sharethewarmth.php
+++ sharethewarmth.php
@@ -15,3 +15,11 @@ require_once 'templates/sharethewarmth_cart.php';
 require_once 'templates/sharethewarmth_checkout.php';
 require_once 'templates/sharethewarmth_single.php';
 require_once 'templates/sharethewarmth_order-received.php';
+
+
+// enqueue the stylesheet
+function fred_victor_user_scripts() {
+    $plugin_url = plugin_dir_url( __FILE__ );
+    wp_enqueue_style( 'fred_victor_style',  $plugin_url . "/assets/css/style.css");
+}
+add_action( 'wp_enqueue_scripts', 'fred_victor_user_scripts' );
