4455d8d57a997a21f665fc90c3c285c5adbe093d
Sierra Katrian <sierra@autismontario.com>
2020-11-14 11:45:45 -0500

    Updated folders, added css and templates.

---
+++ sharethewarmth.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Share the Warmth
+Plugin URI: https://www.fredvictor.org/
+Description:
+Author: DFG Team
+Version: 1.0
+Author URI: https://www.fredvictor.org/
+*/
+
+
+
+require_once 'templates/sharethewarmth_archive.php';
+require_once 'templates/sharethewarmth_cart.php';
+require_once 'templates/sharethewarmth_checkout.php';
+require_once 'templates/sharethewarmth_single.php';
+require_once 'templates/sharethewarmth_order-received.php';
