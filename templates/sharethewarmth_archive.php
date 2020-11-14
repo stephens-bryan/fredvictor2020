b68e993064a2df6f5b5a677a4e70a051081d8b11
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-11-14 17:51:52 -0500

    added category link func

--- templates/sharethewarmth_archive.php
+++ templates/sharethewarmth_archive.php
@@ -1,22 +1,40 @@
 <?php
 
-add_filter( 'body_class','my_body_classes2' );
-function my_body_classes2( $classes ) {
+add_filter( 'body_class','stw_woo_shop_classes' );
+function stw_woo_shop_classes( $classes ) {
 
-if ( is_product() ) {
 
-    global $post;
-    $terms = get_the_terms( $post->ID, 'product_cat' );
-    var_dump( $terms );
-
-    foreach ($terms as $term) {
-        $product_cat_id = $term->term_id;
-        $classes[] = 'product-in-cat-' . $product_cat_id;    
-    }
-}
-
-if( is_shop() ){
+if( is_shop() || is_product_category()){
     $classes[] = "woocommerce-shop-page";
 }
 return $classes;
 }
+
+function stw_woo_cat_links(){
+    $orderby = 'name';
+    $order = 'asc';
+    $hide_empty = false ;
+    $cat_args = array(
+        'orderby'    => $orderby,
+        'order'      => $order,
+        'hide_empty' => $hide_empty,
+    );
+    $product_categories = get_terms( 'product_cat', $cat_args );
+    if( !empty($product_categories) ){
+        echo '
+    <ul class="gift-categories">';
+        echo '<li><span class="gift-categories-title">Gift Categories</span></li>';
+        foreach ($product_categories as $key => $category) {
+            echo '
+    <li>';
+            echo '<a href="'.get_term_link($category).'" >';
+            echo $category->name;
+            echo '</a>';
+            echo '</li>';
+        }
+        echo '</ul>
+    ';
+    }
+}
+
+add_action( 'woocommerce_after_shop_loop', 'stw_woo_cat_links', 10 );
