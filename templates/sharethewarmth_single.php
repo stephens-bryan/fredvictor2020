a63639c499f2551c80b590638279ca89d64019e8
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-11-15 09:22:16 -0500

    clean up code, attempt to disable zoom

--- templates/sharethewarmth_single.php
+++ templates/sharethewarmth_single.php
@@ -3,12 +3,9 @@
 add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
 function wcs_custom_get_availability( $availability, $_product ) {
     // Change In Stock Text
-    // var_dump($_product);
     global $product;
 
 	$koostis = $product->get_attribute( 'goal-number' );
-
-	// var_dump($koostis);
 
     if ( $_product->is_in_stock() ) {
         $availability['availability'] = __('Available!', 'woocommerce');
@@ -20,16 +17,25 @@ function wcs_custom_get_availability( $availability, $_product ) {
     return $availability;
 }
 
-add_filter( 'woocommerce_product_tabs', 'yikes_remove_description_tab', 20, 1 );
+add_filter( 'woocommerce_product_tabs', 'stw_woo_single_rmv_description', 20, 1 );
 
-function yikes_remove_description_tab( $tabs ) {
+function stw_woo_single_rmv_description( $tabs ) {
 
 	// Remove the description tab
     if ( isset( $tabs['description'] ) ) unset( $tabs['description'] );      	    
     return $tabs;
 }
 
+// Remove product zoom
+// add_action( 'after_setup_theme', 'stw_woo_rmv_product_zoom', 100 );
+add_action( 'wp', 'stw_woo_rmv_product_zoom', 100 );
+function stw_woo_rmv_product_zoom() { 
+    remove_theme_support( 'wc-product-gallery-zoom' );
+    remove_theme_support( 'wc-product-gallery-lightbox' );
+    remove_theme_support( 'wc-product-gallery-slider' );
+}
 
+// Rearrange order of product details
 add_action( 'woocommerce_before_single_product', 'stw_woo_single_product_page' );
 function stw_woo_single_product_page() {
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
@@ -45,20 +51,13 @@ function stw_woo_single_product_page() {
     add_action( 'woocommerce_single_product_summary', 'the_content', 20 );
     add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 30 );
     add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 35 );
-    
-    
-    // add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 10 );
-    // add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
-    // add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 30 );
-    // add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 40 );
 }
 
+// Add back to shop link before single product content
 add_action( 'woocommerce_before_single_product_summary', 'woo_add_continue_shopping_button_to_cart' );
-
 function woo_add_continue_shopping_button_to_cart() {
- $shop_page_url = get_permalink( woocommerce_get_page_id( 'shop' ) );
- 
- echo '<div class="woo-backlink">';
- echo ' <a href="'.$shop_page_url.'" class="">Back to Shop</a>';
- echo '</div>';
+    $shop_page_url = get_permalink( woocommerce_get_page_id( 'shop' ) );
+    echo '<div class="woo-backlink">';
+    echo ' <a href="'.$shop_page_url.'" class="">Back to Shop</a>';
+    echo '</div>';
 }
